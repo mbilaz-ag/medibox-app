1c1733bee7af85cbe696799bbbe385cbd3670949
mbilaz-ag <andrius.grudinskas@gmail.com>
2026-09-08 23:30:16 +0300

    Use UTF-8 bytes in leaflet lookup tests

--- test/medicine_leaflet_lookup_test.dart
+++ test/medicine_leaflet_lookup_test.dart
@@ -18,16 +18,15 @@ void main() {
   test('selects an exact medicine and extracts leaflet sections', () async {
     final client = MockClient((request) async {
       if (request.url.path.startsWith('/paieska/')) {
-        return http.Response('''<div class="card__title"><a href="/espumisan-240mg.html">Espumisan 240 mg minkštosios kapsulės</a></div>
-          <div class="card__title"><a href="/espumisan-40mg.html">Espumisan 40 mg minkštosios kapsulės N25</a></div>''', 200,
-          encoding: utf8);
+        return http.Response.bytes(utf8.encode('''<div class="card__title"><a href="/espumisan-240mg.html">Espumisan 240 mg minkštosios kapsulės</a></div>
+          <div class="card__title"><a href="/espumisan-40mg.html">Espumisan 40 mg minkštosios kapsulės N25</a></div>'''), 200);
       }
       return http.Response('''<main id="modal-info-sheet-content">
         <p>Espumisan 40 mg minkštosios kapsulės</p>
         <article data-nodeindex="1"><p>Kas yra vaistas ir kam vartojamas. ${List.filled(90, 'a').join()}</p></article>
         <article data-nodeindex="2"><p>Įspėjimai. ${List.filled(90, 'b').join()}</p></article>
         <article data-nodeindex="3"><p>Kaip vartoti. ${List.filled(90, 'c').join()}</p></article>
-      </main>''', 200, encoding: utf8);
+      </main>'''), 200);
     });
     final result = await MedicineLeafletLookup(client).find(medicine);
     expect(result.url, 'https://vaistai.lt/espumisan-40mg.html');
@@ -35,9 +34,8 @@ void main() {
   });
 
   test('does not confuse 40 mg with 240 mg', () async {
-    final client = MockClient((_) async => http.Response(
-      '<div class="card__title"><a href="/wrong">Espumisan 240 mg minkštosios kapsulės</a></div>', 200,
-      encoding: utf8));
+    final client = MockClient((_) async => http.Response.bytes(utf8.encode(
+      '<div class="card__title"><a href="/wrong">Espumisan 240 mg minkštosios kapsulės</a></div>'), 200));
     await expectLater(MedicineLeafletLookup(client).find(medicine), throwsFormatException);
   });
 }
